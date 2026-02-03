8b467741b7bb7bb9da2013d6dfe6a4c8c82419a6
neeluk7 <neelukalani7@gmail.com>
2026-02-03 13:24:57 +0100

    Patch for running FlashPoint on FPGA

--- run_build.sh
+++ run_build.sh
@@ -1,4 +1,7 @@
 cd opensbi-stage1
+# Make clean doesn't erase everything in the build... so if you change FW_TEXT_ADDR or FW_PAYLOAD_OFFSET, then do rm -rf build/* or update the Makefile as needed.
+export CROSS_COMPILE=riscv64-linux-gnu-
 make clean
-make PLATFORM=generic TYCHE_SM_PATH=../target/riscv-unknown-kernel/release/tyche FW_PAYLOAD=y FW_PAYLOAD_PATH=../builds/linux-riscv/arch/riscv/boot/Image -j $(nproc)
+#make PLATFORM=generic TYCHE_SM_PATH=../target/riscv-unknown-kernel/release/tyche FW_PAYLOAD=y FW_PAYLOAD_PATH=../builds/linux-riscv/arch/riscv/boot/Image -j $(nproc)
+make PLATFORM=generic TYCHE_SM_PATH=../target/riscv-unknown-kernel/release/tyche FW_PAYLOAD=y FW_PAYLOAD_PATH=/mnt/ssd4t/neelu/Image-XS -j $(nproc)
 cd ..
